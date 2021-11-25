7d89b6680189a4e3f1b04a575d3502908126a0ec
SathwikaPodugu <93912402+SathwikaPodugu@users.noreply.github.com>
2021-11-25 10:04:43 +0530

    Create  if else .c

---
+++ if else .c
@@ -0,0 +1,17 @@
+#incluse<stdio.h>
+intmain()
+{
+  int a;
+printf("enter the value a");
+scanf("%d',a);
+if
+{
+ a%2=0;
+ printf(" even number");
+}
+else
+{
+  printf("odd number");
+}
+ return 0;
+ }
